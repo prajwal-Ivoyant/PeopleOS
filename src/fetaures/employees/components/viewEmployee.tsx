1bc3c6899d59d7bdfd8ff818bca45a932d128d68
prajwal-Ivoyant <prajwalp@ivoyant.com>
2026-03-29 21:08:42 +0530

    Update viewEmployee.tsx

--- src/fetaures/employees/components/viewEmployee.tsx
+++ src/fetaures/employees/components/viewEmployee.tsx
@@ -114,7 +114,7 @@ const ViewEmployee: React.FC<ViewEmployeeProps> = ({
 
     const handleCancelEdit = () => {
         form.resetFields();
-        setIsFormChanged(false);
+
         setIsEditing(false);
     };
 
